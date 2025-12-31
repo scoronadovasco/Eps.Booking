5ebbdbedfb29777075762f47fd60a9333419ce3b
scoronadovasco <scoronadovasco@gmail.com>
2025-12-31 17:13:09 -0500

    Refactor AppointmentEntity to reorder properties for better readability

--- src/Eps.Booking.Domain/Entities/Appointment/AppointmentEntity.cs
+++ src/Eps.Booking.Domain/Entities/Appointment/AppointmentEntity.cs
@@ -6,14 +6,14 @@ namespace Eps.Booking.Domain.Entities;
 public class AppointmentEntity
 {
     public Guid Id { get; set; }
+    public DateTime StartDateTime { get; set; }
+    public DateTime EndDateTime { get; set; }
+    public AppointmentStatus Status { get; set; }
+    public DateTime CreatedAt { get; set; } = DateTime.UtcNow;
     public Guid DoctorId { get; set; }
     public DoctorEntity Doctor { get; set; } = default!;
     public Guid PatientId { get; set; }
     public PatientEntity Patient { get; set; } = default!;
     public Guid ClinicId { get; set; }
     public ClinicEntity Clinic { get; set; } = default!;
-    public DateTime StartDateTime { get; set; }
-    public DateTime EndDateTime { get; set; }
-    public AppointmentStatus Status { get; set; }
-    public DateTime CreatedAt { get; set; } = DateTime.UtcNow;
 }
