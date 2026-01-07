9e1f69cc5944eebe25aca1968e664dda1c07a627
scoronadovasco <scoronadovasco@gmail.com>
2026-01-06 21:24:49 -0500

    Add CreateAt property to DoctorEntity and PatientEntity; update migration files and model snapshot

---
+++ src/Eps.Booking.Persistence/Migrations/20260107015020_setdate_doctor_patient.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Eps.Booking.Persistence.Migrations
+{
+    /// <inheritdoc />
+    public partial class setdate_doctor_patient : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<DateTime>(
+                name: "CreateAt",
+                table: "Patients",
+                type: "datetime2",
+                nullable: false,
+                defaultValue: new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified));
+
+            migrationBuilder.AddColumn<DateTime>(
+                name: "CreateAt",
+                table: "Doctors",
+                type: "datetime2",
+                nullable: false,
+                defaultValue: new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified));
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "CreateAt",
+                table: "Patients");
+
+            migrationBuilder.DropColumn(
+                name: "CreateAt",
+                table: "Doctors");
+        }
+    }
+}
