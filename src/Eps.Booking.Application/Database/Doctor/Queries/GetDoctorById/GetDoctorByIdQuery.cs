17c2fc21a5e4d5aa24375de63dfe4f76befbe072
scoronadovasco <scoronadovasco@gmail.com>
2026-01-06 22:26:58 -0500

    Fix formatting and access modifier for GetDoctorByIdQuery constructor and method

--- src/Eps.Booking.Application/Database/Doctor/Queries/GetDoctorById/GetDoctorByIdQuery.cs
+++ src/Eps.Booking.Application/Database/Doctor/Queries/GetDoctorById/GetDoctorByIdQuery.cs
@@ -3,20 +3,20 @@ using Microsoft.EntityFrameworkCore;
 
 namespace Eps.Booking.Application.DataBase.Doctor.Querys;
 
-public class GetDoctorByIdQuery: IGetDoctorByIdQuery
+public class GetDoctorByIdQuery : IGetDoctorByIdQuery
 {
     private readonly IDataBaseService _dataBaseService;
     private readonly IMapper _mapper;
 
-    GetDoctorByIdQuery(IDataBaseService dataBaseService, IMapper mapper)
+    public GetDoctorByIdQuery(IDataBaseService dataBaseService, IMapper mapper)
     {
         _dataBaseService = dataBaseService;
         _mapper = mapper;
     }
 
-    public async Task<GetDoctorByIdModel>Execute(Guid id)
+    public async Task<GetDoctorByIdModel> Execute(Guid id)
     {
-        var entity =await _dataBaseService.Doctors.FirstOrDefaultAsync(x => x.Id == id);
+        var entity = await _dataBaseService.Doctors.FirstOrDefaultAsync(x => x.Id == id);
         return _mapper.Map<GetDoctorByIdModel>(entity);
     }
 }
