cecd5e347e4a19a3d375e744cd76c0396334166a
scoronadovasco <scoronadovasco@gmail.com>
2026-01-13 19:18:19 -0500

    Add ResponseApiService and BaseResponseModel for standardized API responses

---
+++ src/Eps.Booking.Application/Features/ResponseApiService.cs
@@ -0,0 +1,24 @@
+using Eps.Booking.Domain.Models;
+
+namespace Eps.Booking.Application.Features;
+
+public static class ResponseApiService
+{
+    public static BaseResponseModel Response(int statuscode, object data = null, string message = null)
+    {
+        bool success = false;
+
+        if (statuscode >= 200 && statuscode < 300)
+            success = true;
+
+        var result = new BaseResponseModel
+        {
+            StatusCode = statuscode,
+            Success = success,
+            Message = message,
+            Data = data
+        };
+
+        return result;
+    }
+}
---
+++ src/Eps.Booking.Domain/Models/BaseResponseModel.cs
@@ -0,0 +1,9 @@
+namespace Eps.Booking.Domain.Models;
+
+public class BaseResponseModel
+{
+    public int StatusCode { get; set; }
+    public bool Success { get; set; }
+    public string Message { get; set; }
+    public dynamic Data { get; set; }
+}
